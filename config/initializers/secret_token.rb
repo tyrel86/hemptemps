f6b249723964abe35bf1b24d4390c0cfb208ab57
tyrel86 <tyrelrichey@gmail.com>
2013-06-03 01:14:04 -0600

    JKL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hemptemps::Application.config.secret_token = '444a45096bc4ff4e3c9ed6da7775c13dd3409580fd7b332114087e62cb1649d50c2b7875aef987da579b5174641ec06ce89b5f2293b575bafeade589ec55376a'
